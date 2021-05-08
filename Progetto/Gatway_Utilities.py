2fabc645cdb85fa38a3f0d257510ab36516ce364
Alex <alex.testa@studio.unibo.it>
2021-05-08 22:10:21 +0200

    Add timeout to gatway server

--- Progetto/Gatway_Utilities.py
+++ Progetto/Gatway_Utilities.py
@@ -5,25 +5,36 @@
 
 import socket as sk
 
+def replace_ip_in_string(ip, string):
+    string = string.replace(string.split()[0], ip)
+    return string
 
-def tcp_send_message(server_port_tcp, message):
+def print_log_file(path, data):
+    fout = open(path, 'a')
+    if fout.write('\n' + data):
+        return True
+    fout.close()
+    return False
+
+
+def tcp_send_message(server_port_tcp, message, path_log_file='GatwayLog/log.txt'):
     
     tcp_socket = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
     
     try:
+        tcp_socket.settimeout(10)
         tcp_socket.connect(('localhost', server_port_tcp))
+        tcp_socket.send(message.encode('utf8'))
+        response = tcp_socket.recv(server_port_tcp)    
+         
+        print(' Central Server Response : ' + response.decode('utf8'))
+    
+        tcp_socket.close()
     except Exception as data:
         print(' fail : ', data)
         print(' failed connection')
+        print(' Store data ')
+        print_log_file(path_log_file, message)
         tcp_socket.close()
     
-    tcp_socket.send(message.encode('utf8'))
-    response = tcp_socket.recv(server_port_tcp)    
-    print(' Central Server Response : ' + response.decode('utf8'))
-    
-    tcp_socket.close()
     return
-
-def replace_ip_in_string(ip, string):
-    string = string.replace(string.split()[0], ip)
-    return string
